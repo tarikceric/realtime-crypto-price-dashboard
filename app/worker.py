9959425f176a67fbe4c480bc516c355481a73a55
tarikceric <tarceric@gmail.com>
2022-06-21 19:36:03 -0700

    update for Binance API

--- app/worker.py
+++ app/worker.py
@@ -1,32 +1,21 @@
-import finnhub
+# import finnhub
 from celery import Celery
 from sqlalchemy import text
 from app.db import engine
 from app.settings import settings
+from binance import Client
 
-client = finnhub.Client(api_key=settings.api_key)
+
+# configure the Binance client and Celery to use the Redis broker defined in the application settings
+client = Client(api_key=settings.api_key, api_secret=settings.api_secret)
 celery_app = Celery(broker=settings.celery_broker)
 
 
 # worker is started from the root of the project, command line:
 # python -m celery --app app.worker.celery_app worker --beat -l info -c 1
-#
-# worker relies on this table to exist:
-#
-# https://questdb.io/docs/reference/sql/create-table/#symbol
-# CREATE TABLE
-#      quotes(stock_symbol SYMBOL CAPACITY 5 CACHE INDEX, -- we are in fact just checking 3
-#             current_price DOUBLE,
-#             high_price DOUBLE,
-#             low_price DOUBLE,
-#             open_price DOUBLE,
-#             percent_change DOUBLE,
-#             tradets TIMESTAMP, -- timestamp of the trade
-#             ts TIMESTAMP)      -- time of insert in our table
-#      timestamp(ts)
-#  PARTITION BY DAY;
 
-
+# Create a periodic task for each symbol defined in the settings.
+# This registers a new periodic per symbol after Celery is connected to the broker
 @celery_app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     """
@@ -39,41 +28,41 @@ def setup_periodic_tasks(sender, **kwargs):
 @celery_app.task
 def fetch(symbol: str):
     """
-    Fetch the stock info for a given symbol from Finnhub and load it into QuestDB.
+    Fetch the stock info for a given symbol from Binance and load it into QuestDB.
     """
 
-    quote: dict = client.quote(symbol)
-    # https://finnhub.io/docs/api/quote
-    #  quote = {
-    #      'c': 148.96,
-    #      'd': -0.84,
-    #      'dp': -0.5607,
-    #      'h': 149.7,
-    #      'l': 147.8,
-    #      'o': 148.985,
-    #      'pc': 149.8,
-    #      't': 1635796803
-    #  }
-    # c: Current price
-    # d: Change
-    # dp: Percent change
-    # h: High price of the day
-    # l: Low price of the day
-    # o: Open price of the day
-    # pc: Previous close price
-    # t: when it was traded
+    quote = client.get_klines(symbol=symbol, interval=client.KLINE_INTERVAL_1MINUTE, limit=1)
 
-    # I wonder if these inserts could be batched
+
+    # https://docs.binance.us/?python#get-candlestick-data
+    # quote =
+    # [
+    #   [
+    #     1499040000000,      // Open time
+    #     "0.00386200",       // Open
+    #     "0.00386200",       // High
+    #     "0.00386200",       // Low
+    #     "0.00386200",       // Close
+    #     "0.47000000",  // Volume
+    #     1499644799999,      // Close time
+    #     "0.00181514",    // Quote asset volume
+    #     1,                // Number of trades
+    #     "0.47000000",    // Taker buy base asset volume
+    #     "0.00181514",      // Taker buy quote asset volume
+    #     "0" // Ignore.
+    #   ]
+    # ]
+
     query = f"""
-    INSERT INTO quotes(stock_symbol, current_price, high_price, low_price, open_price, percent_change, tradets, ts)
+    INSERT INTO quotes(stock_symbol, current_price, high_price, low_price, open_price, num_trades, tradets, ts)
     VALUES(
         '{symbol}',
-        {quote["c"]},
-        {quote["h"]},
-        {quote["l"]},
-        {quote["o"]},
-        {quote["pc"]},
-        {quote["t"]} * 1000000,
+        {quote[0][4]},
+        {quote[0][2]},
+        {quote[0][3]},
+        {quote[0][1]},
+        {quote[0][8]},
+        {quote[0][6]} * 1000,
         systimestamp()
     );
     """
